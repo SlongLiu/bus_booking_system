3fa23f52bb4e3e6cde10379847ccdce1075c47d8
Slong Liu <long199886@163.com>
2019-01-18 02:06:27 +0800

    Initial commit

---
+++ booking/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils import timezone
+from django import forms
+# Create your models here.
+
+
+class Message(models.Model):
+    '''
+    用户消息
+    '''
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class User(models.Model):
+    '''
+    用户信息
+    '''
+
+    gender = (
+        ('male', '男'),
+        ('female', '女'),
+    )
+
+    name = models.CharField(max_length=128, unique=True)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(unique=True)
+    sex = models.CharField(max_length=32, choices=gender, default='男')
+    c_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['c_time']
+        verbose_name = '用户'
+        verbose_name_plural = '用户'
+
+
+class UserForm(forms.Form):
+    '''
+    创建表单模型
+    '''
+    username = forms.CharField(label="用户名", max_length=128)
+    password = forms.CharField(label="密码", max_length=256, widget=forms.PasswordInput)
