2671865074252274052baf37ba404cb9af8adb7d
Slong Liu <long199886@163.com>
2019-01-18 03:40:27 +0800

    V0.1.2
    稍微修改了数据库，但是没改完

--- booking/models.py
+++ booking/models.py
@@ -25,6 +25,8 @@ class User(models.Model):
         ('female', '女'),
     )
 
+    usertype = ('user', 'busdriver', 'employee')
+
     name = models.CharField(max_length=128, unique=True)
     password = models.CharField(max_length=256)
     email = models.EmailField(unique=True)
@@ -38,4 +40,47 @@ class User(models.Model):
         ordering = ['c_time']
         verbose_name = '用户'
         verbose_name_plural = '用户'
+
+
+class Line(models.Madel):
+    '''
+    线路信息
+    '''
+
+    name = models.CharField(max_length=256, unique=True)
+    destination = models.CharField(max_length=256)
+    service_time_start = models.TimeField()
+    service_time_end = models.TimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class PriceOfLine(models.Model):
+    '''
+    票价信息
+    '''
+
+    line = models.ForeignKey(to='Line', to_field='name')
+    site = models.CharField(max_length=256)
+    price = models.FloatField()
+
+
+class Shuttle(models.Model):
+    '''
+    大巴信息
+    '''
+
+    line = models.ForeignKey(to='Line', to_field='name')
+    plate = models.CharField(max_length=64, unique=True)
+
+
+class Departure(models.Model):
+    '''
+    车的班次信息
+    '''
+
+    shuttle_id = models.ForeignKey(to='Shuttle', to_field='id')
+    datetime = models.DateTimeField()
+    busdriver_id = models.ForeignKey(to='User', to_field='id')
 
